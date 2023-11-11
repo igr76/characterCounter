4d8357a5309240ae1a6439d050990aee51a36eb8
igr76 <gritsuk766@gmail.com>
2023-11-12 08:53:05 +1000

    доработка сервиса.

--- src/test/java/com/charactercounter/controller/CounterControllerTest.java
+++ src/test/java/com/charactercounter/controller/CounterControllerTest.java
@@ -9,15 +9,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CounterController.class)
 public class CounterControllerTest {
     @Autowired
     private MockMvc mockMvc;
-   @InjectMocks
-    private CounterController counterController ;
    @MockBean
    private CounterService counterService;
 
@@ -25,6 +26,7 @@ public class CounterControllerTest {
     void getCharacterCounter() throws Exception {
         String url = "/aabccc";
         mockMvc.perform(get(url)
+                        .content("c:3,a:2,b:1")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
